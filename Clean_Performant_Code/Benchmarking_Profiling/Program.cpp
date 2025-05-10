4c42c82a1103ce424fb799e13b2fd2f87443b2c8
Peter Loos <peter.loos@gmx.de>
2025-05-10 18:14:38 +0200

    added some more benchmarks

--- Clean_Performant_Code/Benchmarking_Profiling/Program.cpp
+++ Clean_Performant_Code/Benchmarking_Profiling/Program.cpp
@@ -2,14 +2,12 @@
 // Program.cpp // Benchmarking // Profiling
 // ===========================================================================
 
-extern void performance_performing_benchmarks();
 extern void performance_profiling_puzzle_10();
 extern void performance_profiling_puzzle_11();
 
 int main()
 {
-    //performance_performing_benchmarks();
-    //performance_profiling_puzzle_10();
+    performance_profiling_puzzle_10();
     performance_profiling_puzzle_11();
     return 0;
 }
