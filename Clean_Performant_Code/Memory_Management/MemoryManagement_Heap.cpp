214a953296cb0785e4291722cb994507a6a7e571
Peter Loos <peter.loos@gmx.de>
2025-12-17 13:02:37 +0100

    continued work

--- Clean_Performant_Code/Memory_Management/MemoryManagement_Heap.cpp
+++ Clean_Performant_Code/Memory_Management/MemoryManagement_Heap.cpp
@@ -2,12 +2,16 @@
 // MemoryManagement_Heap.cpp // Memory Management
 // ===========================================================================
 
+#include <cstdlib>
 #include <new>
 #include <print>
 
 namespace MemoryManagement {
 
+
     namespace Using_New_delete {
+
+        // =======================================================================
 
         class Foo
         {
@@ -23,9 +27,11 @@ namespace MemoryManagement {
             int* p = new int(123);   // dynamically allocate one int
             delete p;                // deallocate it
 
-            Foo* f = new Foo(123);
+            Foo* f = new Foo{ 123 }; // same for class type
             delete f;
         }
+
+        // =======================================================================
 
         static void test_02_arrays() {
 
@@ -48,6 +54,7 @@ namespace MemoryManagement {
 
         // https://en.cppreference.com/w/cpp/memory/new/nothrow.html
 
+        // =======================================================================
 
         static void test_03_nothrow_new_01() {
 
@@ -60,42 +67,100 @@ namespace MemoryManagement {
             delete p;
         }
 
-        static void test_03_nothrow_new_01() {
+        static void test_03_nothrow_new_02() {
+
+            constexpr unsigned long long Size = 1'000'000'000ul;
 
             try
             {
                 while (true)
                 {
-                    new int[100000000ul];   // throwing overload
+                    new int[Size];   // throwing overload
+                    std::println("Allocation new int succeeded.");
                 }
             }
             catch (const std::bad_alloc& e)
             {
-                std::cout << e.what() << '\n';
+                std::println("Exception: {}", e.what());
             }
 
             while (true)
             {
-                int* p = new(std::nothrow) int[100000000ul]; // non-throwing overload
+                int* p = new(std::nothrow) int[Size]; // non-throwing overload
                 if (p == nullptr)
                 {
-                    std::cout << "Allocation returned nullptr\n";
+                    std::println("Allocation returned nullptr");
                     break;
+                }
+                else {
+                    std::println("Allocation new(std::nothrow) succeeded.");
                 }
             }
         }
 
-
-
         static void test_03_nothrow_new() {
+
+            test_03_nothrow_new_01();
+            test_03_nothrow_new_02();
+        }
+
+        // =======================================================================
+
+        static void test_04_value_initialization_vs_default_initialization() {
+
+            // Default-Initialization
+            int* p1 = new int;         // uninitialized value
+            delete p1;
+
+            // Value-initialization
+            int* p2 = new int{};       // initialized to 0
+            delete p2;
+
+            // Default-Initialization
+            Foo* f1 = new Foo;         // default constructor
+            delete f1;
+
+            // Value-initialization
+            Foo* f2 = new Foo{};       // also default constructor
+            delete f2;
+        }
+
+        // =======================================================================
+
+        // class-specific allocation
+
+        class AnotherFoo
+        {
+        public:
+            static void* operator new(std::size_t size) {
+                std::println("operator new: >>> {} bytes");
+                return std::malloc(size);
+            }
+
+            static void operator delete(void* ptr) {
+                std::println("operator delete: >>> {} ptr");
+                std::free(ptr);
+            }
+        };
+
+        static void test_05_custom_new_custom_delete() {
+
+            AnotherFoo* f = new AnotherFoo;
+            delete f;
+
         }
 
     }
 }
 
-
 void memory_management_heap()
 {
+    using namespace MemoryManagement;
+
+    Using_New_delete::test_01_single_object();
+    Using_New_delete::test_02_arrays();
+    // Using_New_delete::test_03_nothrow_new();
+    Using_New_delete::test_04_value_initialization_vs_default_initialization();
 }
 
 // ===========================================================================
