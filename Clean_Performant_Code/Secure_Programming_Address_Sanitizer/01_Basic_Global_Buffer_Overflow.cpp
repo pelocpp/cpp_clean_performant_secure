a6c3e5ceb4b0bab5ab8b8883df5a55f6a18a7370
Peter Loos <peter.loos@gmx.de>
2026-05-16 18:53:57 +0200

    minor changes

--- Clean_Performant_Code/Secure_Programming_Address_Sanitizer/01_Basic_Global_Buffer_Overflow.cpp
+++ Clean_Performant_Code/Secure_Programming_Address_Sanitizer/01_Basic_Global_Buffer_Overflow.cpp
@@ -1,5 +1,5 @@
 // ===========================================================================
-// Program.cpp // InfInt // Arbitrary-sized integer class for C++
+// 01_Basic_Global_Buffer_Overflow.cpp // Address Sanitizer
 // ===========================================================================
 
 extern void infint_examples();
@@ -8,6 +8,21 @@ int main()
 {
     infint_examples();
     return 0;
+}
+
+// 02_Alloc_Dealloc_Mismatch.cpp
+
+#include <print>
+
+int buffer[20];
+
+void test_01_basic_global_buffer_overflow()
+{
+    std::println("Hello Global Buffer Overflow:");
+
+    std::size_t n{ 20 };
+
+    buffer[n] = 123; // Boom!
 }
 
 // ===========================================================================
